ef86911c26822f9e589f358973536de6227a38c1
kalys <61675998+kalyskhairy@users.noreply.github.com>
2020-05-12 02:18:26 +0700

    commit

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,6 +2,8 @@ function errorHandler(err, req, res , next){
     console.log(err)
     if(err.name == 'JsonWebTokenError'){
         res.status(401).json({msg : 'Login first'})
+    }else if(err.name == 'SequelizeValidationError'){
+        res.status(400).json({error : err.msg})
     }else{
         res.status(err.code || 500).json({error : err.msg})
     }
--- test/user.test.js
+++ test/user.test.js
@@ -1,10 +1,13 @@
 const app = require('../app')
 const request = require('supertest')
+const {sequelize} = require('../models')
+const {queryInterface} = sequelize
 
 
 describe('User Router', () => {
     beforeAll(function() {
         jest.setTimeout(10000);
+        queryInterface.bulkDelete('Users')
     });
     describe('register', () => {
         describe('Success : ', () => {
@@ -29,5 +32,81 @@ describe('User Router', () => {
                 })
             })
         })
+
+        describe('Fail : ', () => {
+            test('should return JSON with id and email with status 500', done => {
+                const userTest = {
+                    email : 'test@mail.com',
+                    password : 'test'
+                }
+                request(app)
+                    .post('/users/register')
+                    .send(userTest)
+                    .end((err, response) => {
+                        if(err) {
+                            return done(err)
+                        }else{
+                            expect(response.status).toBe(500)
+                            expect((result) => {
+                                let data = result.body
+                                expect(data.message).toContain('Password must be 5 between 16')
+                            })
+                            return done()
+                        }
+                })
+            })
+        })
+
+        describe('Fail : ', () => {
+            test('should return JSON with id and email with status 500', done => {
+                const userTest = {
+                    email : 'testmailcom',
+                    password : 'testTING'
+                }
+                request(app)
+                    .post('/users/register')
+                    .send(userTest)
+                    .end((err, response) => {
+                        if(err) {
+                            return done(err)
+                        }else{
+                            expect(response.status).toBe(500)
+                            expect((result) => {
+                                let data = result.body
+                                expect(data.message).toContain('Invalid Email Format')
+                            })
+                            return done()
+                        }
+                })
+            })
+        })
+
+        describe('Fail : ', () => {
+            test('should return JSON with id and email with status 500', done => {
+                const userTest = {
+                    email : '',
+                    password : ''
+                }
+                request(app)
+                    .post('/users/register')
+                    .send(userTest)
+                    .end((err, response) => {
+                        if(err) {
+                            return done(err)
+                        }else{
+                            expect(response.status).toBe(500)
+                            expect((result) => {
+                                let data = result.body
+                                expect(data.message).toContain('Email is required')
+                            })
+                            return done()
+                        }
+                })
+            })
+        })
+    })
+    afterAll(function() {
+        queryInterface.bulkDelete('Users')
     })
 })
+
