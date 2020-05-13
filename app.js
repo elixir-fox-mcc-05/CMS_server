b8b444af9f4f9e80eef3441485557513896315ac
yodji09 <yodji09@gmail.com>
2020-05-13 18:34:47 +0700

    Deploy

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const port = process.env.PORT || 3000;
 const cors = require('cors');
 const errHandler = require('./middlewares/errHandler');
 const router = require('./routes/index');
-require('dotenv').config();
 
 app.use(cors());
 app.use(express.urlencoded({extended : true}));
