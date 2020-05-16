574374e77debdc68c4a84ae9f573b9f480b2a8db
SackingSand <seratusrb@gmail.com>
2020-05-16 19:05:57 +0700

    cors and dotenv not saved to package.json fix

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express()
 const cors = require(`cors`);
 const router = require(`./routers`);
 
-app.use(cors())
 app.use(express.urlencoded({ extended : false}))
 app.use(express.json())
+app.use(cors())
 
 app.use(router)
 
