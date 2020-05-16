b750b8fd728679121ab5a56a5c19ec0aba6177d9
hmyslf <yosa.alfq4@gmail.com>
2020-05-16 12:47:24 +0700

    deploy

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ const userRouter = require('./user.js');
 const productRouter = require('./product.js');
 const categoryRouter = require('./category.js');
 
+router.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'CMS Server is running'
+  })
+});
 router.use('/', userRouter);
 router.use('/products', productRouter);
 router.use('/categories', categoryRouter);
