e93411ace7fb5fd6216199cd4d10a881673df5c0
Ottoyd <ugaipribadi@gmail.com>
2020-05-16 20:35:10 +0700

    'cloud'

--- routers/user.js
+++ routers/user.js
@@ -1,6 +1,9 @@
 let router = require('express').Router()
 let userController = require('../controllers/user')
 
+router.get((req, res) => {
+  res.send('HEROKUUU')
+}) 
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 
