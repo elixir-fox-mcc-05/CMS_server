f811bd17546754ecc37b771efaf9a058f905b5f2
AndreRGunawan <andre.graphica@gmail.com>
2020-05-16 17:08:16 +0700

    added deploy code and credentials

--- routers/index.js
+++ routers/index.js
@@ -3,6 +3,12 @@ const router = express.Router()
 const userRoutes = require("../routers/userRoutes.js")
 const productRoutes = require("../routers/ProductRouters.js")
 
+router.get('/', function(req, res) {
+    res.status(200).json({
+        message: 'Hop & Lop CMS Domain Connected'
+    })
+})
+
 router.use('/', userRoutes)
 router.use('/products', productRoutes)
 
