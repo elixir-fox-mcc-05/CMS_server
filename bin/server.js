669c76ff45363681f474aa7ba2d9e412193b00e7
achrams <ramadhanovic@gmail.com>
2020-05-22 22:17:03 +0700

    server fix

--- bin/server.js
+++ bin/server.js
@@ -6,19 +6,6 @@ const server = http.createServer(app)
 const io = require('socket.io')(server)
 const { Product } = require('../models')
 
-io.on('connection', (socket)=>{
-  console.log('A user Connected')
-
-  socket.on('transaction', () => {
-    console.log('A transaction has been completed')
-    Product.findAll()
-    .then(data => {
-      io.emit('finish-transaction', data)
-    })
-  })
-
-})
-
 server.listen(PORT, () => {
   console.log(`PORT : ${PORT}`)
 })
