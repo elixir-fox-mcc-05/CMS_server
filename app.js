54e2f6daf8e6cea98614dd9327f75c12340d2dfe
AndreRGunawan <andre.graphica@gmail.com>
2020-06-03 23:56:37 +0700

    tweak access origin code

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 
 const routes = require("./routers/index.js")
 
-app.all('/', function(req, res, next) {
+app.use('/', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next()
