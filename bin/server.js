b50d7318a02aaf7f19a693405a1c222979b40b42
AndreRGunawan <andre.graphica@gmail.com>
2020-06-03 20:33:26 +0700

    add

--- bin/server.js
+++ bin/server.js
@@ -1,5 +1,5 @@
 const app = require('../app'); 
-const port = process.env.PORT // process.env.PORT ini kita tambahkan sebagai alternatif untuk proses deploy heroku
+const port = process.env.PORT //process.env.PORT ini kita tambahkan sebagai alternatif untuk proses deploy heroku
 
 const http = require('http'); // Line 4 dan 5 ini dilakukan sebagai standard untuk mempermudah migrasi ke 'https'
 const server = http.createServer(app);
