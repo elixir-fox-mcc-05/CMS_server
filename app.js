578dd640f24d1ddcc8223b19f57c5d8ed2983981
AndreRGunawan <andre.graphica@gmail.com>
2020-06-03 20:19:13 +0700

    remove dotenv setting

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+// require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000 // Port tidak kita state di sini karena kita akan gunakan server di bin
