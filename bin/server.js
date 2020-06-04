e334239a81afe58187dd57fa576f03debf4b12a4
ahmadalhq <ahmadalhq@gmail.com>
2020-06-04 15:46:51 +0700

    fix bug deploy

--- bin/server.js
+++ bin/server.js
@@ -4,6 +4,6 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app)
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Connected to port ${port}`);
 });
