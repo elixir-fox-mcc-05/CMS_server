2632f226347c9af92754b63b37cb2d04618b33cf
amirfaisalz <amirsakti27@gmail.com>
2020-06-04 15:49:14 +0700

    trying no 3

--- seeders/20200604081752-add-super-admin.js
+++ seeders/20200604081752-add-super-admin.js
@@ -14,7 +14,7 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
-   return queryInterface.bulkInsert('Users', {
+   return queryInterface.bulkInsert('Users', [{
     name: 'Amir Faisal Z',
     email: "amir@gmail.com",
     role: "Super-admin",
@@ -22,7 +22,7 @@ module.exports = {
     image_url: "https://b1.pngbarn.com/png/250/817/francisco-lachowski-smiling-man-wearing-white-crew-neck-shirt-png-clip-art.png",
     createdAt : new Date(),
     updatedAt : new Date()
-  }, {});
+  }], {});
   },
 
   down: (queryInterface, Sequelize) => {
